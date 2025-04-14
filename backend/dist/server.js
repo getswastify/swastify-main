62d941110c43d0b147919095f8d30dca37dea50f
BeastxD7 <shashankdevadiga2003@gmail.com>
2025-04-14 18:31:12 +0530

    Added Express and Dotenv

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -1,1 +1,17 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const ping_1 = __importDefault(require("./routes/ping"));
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 3000;
+app.use(express_1.default.json());
+app.use('/ping', ping_1.default);
+app.get('/ping', (_req, res) => {
+    res.send('pong');
+});
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
