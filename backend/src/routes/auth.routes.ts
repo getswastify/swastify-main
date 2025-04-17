1489a85d595c3a59122b33913d33367a628820d2
BeastxD7 <shashankdevadiga2003@gmail.com>
2025-04-17 17:20:03 +0530

    removed unused deps & Added Azure Email SDK with register routes

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
-import {  registerPatient, verifyOtpAndRegister } from '../controller/auth.controller';
+import {  registerDoctor, registerHospital, registerUser, verifyOtpAndRegister } from '../controller/auth.controller';
 
 
 const router = Router();
 
-router.post('/register', registerPatient);
+router.post('/register', registerUser);
+router.post('/register/doctor', registerDoctor);
+router.post('/register/hospital', registerHospital);
+
+
 router.post('/verify-otp', verifyOtpAndRegister);
 
 export const authRoutes =  router;
