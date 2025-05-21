5b7b7bc613cb619b84abbd21f97121157591f768
BeastxD7 <shashankdevadiga2003@gmail.com>
2025-05-21 18:01:36 +0530

    Added AI Agent with Book Appointment Tool

---
+++ ai-agent/langgraph-agent/dist/tools/tools.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getAvailableTimeSlots = getAvailableTimeSlots;
+const axios_1 = __importDefault(require("axios"));
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
+async function getAvailableTimeSlots({ doctorId, date }) {
+    var _a;
+    try {
+        const response = await axios_1.default.post(`${process.env.API_URL}/patient/available-slots`, {
+            doctorId,
+            date,
+        });
+        return response.data.availableSlots;
+    }
+    catch (err) {
+        console.error("🛑 Slot API error:", ((_a = err.response) === null || _a === void 0 ? void 0 : _a.data) || err.message);
+        return [];
+    }
+}
